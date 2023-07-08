7b0384035625a932a9471e832d721bbf9cc456d5
yramirel <yelsinkennedy.ramirezlujan@emeal.nttdata.com>
2023-07-08 11:40:13 -0500

    init

---
+++ src/test/java/com/nttdata/microclient/MicroClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.microclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
